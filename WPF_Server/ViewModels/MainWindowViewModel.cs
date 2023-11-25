058323c2b216256f5051aa9ebd68cbd7162e6583
diamondree <kirill00400@gmail.com>
2023-11-25 22:42:57 +0300

    Implemented server starting button

--- WPF_Server/ViewModels/MainWindowViewModel.cs
+++ WPF_Server/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,26 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.ServiceModel;
+using System.Windows.Input;
+using WCF_Shared_Library;
+using WPF_Server.Commands;
 
 namespace WPF_Server.ViewModels
 {
-    internal class MainWindowViewModel : INotifyPropertyChanged
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<string> Messages { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Messages = new ObservableCollection<string>();
+        }
+        
 
         public void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
@@ -20,6 +35,30 @@ namespace WPF_Server.ViewModels
             set { 
                 _Port = value;
                 OnPropertyChanged();
+            }
+        }
+
+        public ICommand StartServer
+        {
+            get
+            {
+                return new DelegateCommand((obj) =>
+                {
+                    Uri address = new Uri("net.tcp://localhost:3030/IChatService");
+
+                    NetTcpBinding binding = new NetTcpBinding();
+                    Type contract = typeof(IChatService);
+                    ChatService service = new ChatService();
+                    ServiceHost host = new ServiceHost(service);
+
+                    host.AddServiceEndpoint(contract, binding, address);
+
+
+                    host.Open();
+                    
+                    Messages.Add("Succesfully started");
+                    OnPropertyChanged("Messages");
+                });
             }
         }
     }
