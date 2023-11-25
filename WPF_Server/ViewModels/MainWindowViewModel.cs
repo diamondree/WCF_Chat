a5fa3936fec0cb8985fe80abcdd563bfc90505ce
diamondree <kirill00400@gmail.com>
2023-11-25 18:21:25 +0300

    Implemented server UI in WPF

---
+++ WPF_Server/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_Server.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int _Port;
+
+        public int Port
+        {
+            get { return _Port; }
+            set { 
+                _Port = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
