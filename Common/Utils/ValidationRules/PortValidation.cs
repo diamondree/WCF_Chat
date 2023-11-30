e124a011344da40d9eb504e845873626dd27bc0d
diamondree <kirill00400@gmail.com>
2023-12-01 02:07:59 +0300

    Implemented port validations

---
+++ Common/Utils/ValidationRules/PortValidation.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Common.Utils.ValidationRules
+{
+    public class PortValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int port;
+            int.TryParse((string)value, out port);
+            if (port < 1 || port > 65535)
+                return new ValidationResult(false, "Port must be between 1 and 65535");
+            return new ValidationResult(true, null);
+        }
+    }
+}
