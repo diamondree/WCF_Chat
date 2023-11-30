d612a8784d7cee636c70e5a15c382e535aabc8eb
diamondree <kirill00400@gmail.com>
2023-12-01 02:35:16 +0300

    Fix bug connecting to server with faulted validation

---
+++ Common/Utils/Converters/IpValidationConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Common.Utils.Converters
+{
+    public class IpValidationConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values.Length > 0)
+            {
+                foreach (var value in values)
+                {
+                    if (value is bool && (bool)value)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
